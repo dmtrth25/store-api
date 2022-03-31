8b548b9553731cd88428d11ff2a1db441d2c9c43
dmtrth25 <vodolazhchenko25@gmail.com>
2022-03-31 22:18:32 +0300

    handle query values

--- controllers/products.js
+++ controllers/products.js
@@ -1,11 +1,19 @@
 const Product = require('../models/product')
 
 const getAllProducts = async (req, res) => {
-   const {featured} = req.query
+   const {featured, company, name} = req.query
    const queryObj = {}
 
    if(featured) {
       queryObj.featured = featured === 'true' ? true : false
+   }
+
+   if(company) {
+      queryObj.company = company
+   }
+
+   if(name) {
+      queryObj.name = name
    }
 
    console.log(queryObj)
@@ -14,15 +22,9 @@ const getAllProducts = async (req, res) => {
 }
 
 const getAllProductsStatic = async (req, res) => {
-   const {featured} = req.query
-   const queryObj = {}
-
-   if(featured) {
-      queryObj.featured = featured === 'true' ? true : false
-   }
-
-   console.log(queryObj)
-   const products = await Product.find(queryObj)
+   const products = await Product.find({
+      name: 'albany tablef'
+   })
    res.status(200).json({products, length: products.length})
 }
 
