fee0d1ce0a8d4544b33d1b0c5743cb93308e3618
dmtrth25 <vodolazhchenko25@gmail.com>
2022-04-01 21:25:21 +0300

    added regex filtering property to name

--- controllers/products.js
+++ controllers/products.js
@@ -1,5 +1,13 @@
 const Product = require('../models/product')
 
+const getAllProductsStatic = async (req, res) => {
+   const search = 'qq' // doesn't a matter. it especially for testing
+   const products = await Product.find({
+      name: { $regex: search, $options: 'i'}
+   })
+   res.status(200).json({products, length: products.length})
+}
+
 const getAllProducts = async (req, res) => {
    const {featured, company, name} = req.query
    const queryObj = {}
@@ -13,7 +21,7 @@ const getAllProducts = async (req, res) => {
    }
 
    if(name) {
-      queryObj.name = name
+      queryObj.name = { $regex: name, $options: 'i'}
    }
 
    console.log(queryObj)
@@ -21,12 +29,6 @@ const getAllProducts = async (req, res) => {
    res.status(200).json({products, length: products.length})
 }
 
-const getAllProductsStatic = async (req, res) => {
-   const products = await Product.find({
-      name: 'albany tablef'
-   })
-   res.status(200).json({products, length: products.length})
-}
 
 module.exports = {
    getAllProducts,
