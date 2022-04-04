7aa340e8758da2af8bf5c37afb10e8609334de2c
dmtrth25 <vodolazhchenko25@gmail.com>
2022-04-04 14:12:29 +0300

    added select fields

--- controllers/products.js
+++ controllers/products.js
@@ -1,12 +1,12 @@
 const Product = require('../models/product')
 
 const getAllProductsStatic = async (req, res) => {
-   const products = await Product.find({}).sort('-price')
+   const products = await Product.find({}).select('name price')
    res.status(200).json({products, length: products.length})
 }
 
 const getAllProducts = async (req, res) => {
-   const {featured, company, name, sort} = req.query
+   const {featured, company, name, sort, fields} = req.query
    const queryObj = {}
 
    if(featured) {
@@ -24,11 +24,18 @@ const getAllProducts = async (req, res) => {
    console.log(queryObj)
    let result = Product.find(queryObj)
 
+   //sort
    if(sort) {
       const sortList = sort.split(',').join(' ')
       result = result.sort(sortList)
    } else {
       result = result.sort('createdAt')
+   }
+
+   //select
+   if(fields) {
+      const fieldsList = fields.split(',').join(' ')
+      result = result.select(fieldsList)
    }
    const products = await result
    res.status(200).json({products, length: products.length})
