1def3cd7668965cc8759351abe4b1413d11b7b3c
dmtrth25 <vodolazhchenko25@gmail.com>
2022-04-04 21:41:27 +0300

    added numeric filters

--- controllers/products.js
+++ controllers/products.js
@@ -1,12 +1,17 @@
 const Product = require('../models/product')
 
 const getAllProductsStatic = async (req, res) => {
-   const products = await Product.find({}).select('name price')
+   const products = await Product
+   .find({price: {$gt: 30}})
+   .sort('price')
+   .select('name price')
+   .limit(10)
    res.status(200).json({products, length: products.length})
 }
 
 const getAllProducts = async (req, res) => {
-   const {featured, company, name, sort, fields} = req.query
+   const {featured, company, name, sort, fields, numericFilters} = req.query
+   console.log(req.query)
    const queryObj = {}
 
    if(featured) {
@@ -21,9 +26,21 @@ const getAllProducts = async (req, res) => {
       queryObj.name = { $regex: name, $options: 'i'}
    }
 
+   if(numericFilters) {
+      const operatorMap = {
+         '>': '$gt',
+         '>=': '$gte',
+         '=': '$eq',
+         '<': '$lt',
+         '<=': '$lte' 
+      }
+      const regEx = /\b(<|>|>=|=|<|<=)\b/g
+      let filters = numericFilters.replace(regEx, (match) => `-${operatorMap[match]}-`)
+      console.log(filters);
+   }
+   
    console.log(queryObj)
    let result = Product.find(queryObj)
-
    //sort
    if(sort) {
       const sortList = sort.split(',').join(' ')
@@ -37,6 +54,13 @@ const getAllProducts = async (req, res) => {
       const fieldsList = fields.split(',').join(' ')
       result = result.select(fieldsList)
    }
+
+   const page = Number(req.query.page) || 1
+   const limit = Number(req.query.limit) || 10
+   const skip = (page - 1) * limit
+
+   result = result.skip(skip).limit(limit)
+
    const products = await result
    res.status(200).json({products, length: products.length})
 }
